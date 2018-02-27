327d9c55d40f435db64de4ec53b2c5b894bf8072
enesoz <csenes1987@gmail.com>
2018-02-27 07:19:30 +0300

    Initial commit

---
+++ src/main/java/com/social/media/messages/MessagesApplication.java
@@ -0,0 +1,12 @@
+package com.social.media.messages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagesApplication.class, args);
+	}
+}
