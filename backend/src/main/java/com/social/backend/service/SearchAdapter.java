f95dd4c2131c6c5e55e6689044a63aece1341886
Enes Ozdemir <csenes1987@gmail.com>
2019-02-14 17:14:10 +0300

    remove reduntant code
    remove reduntant code

--- backend/src/main/java/com/social/backend/service/SearchAdapter.java
+++ backend/src/main/java/com/social/backend/service/SearchAdapter.java
@@ -6,8 +6,5 @@ public abstract class SearchAdapter implements SearchApi {
     protected static final String MESSAGE_SERVICE = "messages";
     protected static final int MESSAGE_SERVICE_PORT = 2222;
 
-    public void print() {
-        System.out.println("beni ara");
-    }
 
 }
